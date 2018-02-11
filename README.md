87d0a3a71e2c793c0f7766fae53265f23a13f610
Uche Chilaka <uche@larcity.com>
2018-02-11 15:11:22 -0500

    recent updates, including favicon

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ To run a build that will publish for github pages (with your next push), run the
 ```
 ./build-for-ghpages
 ```
+
+# Configuring Github Pages
+* Follow the instructions at this URL: https://help.github.com/articles/troubleshooting-custom-domains/#dns-configuration-errors
+* Be sure to set the custom domain to `www.coinstore.co` on github pages
+* Create the following DNS records with your domain registrar:
+    ```
+    coinscore.co        3600    IN      A       192.30.252.153
+    coinscore.co        3600    IN      A       192.30.252.154
+    www.coinscore.co    3600    IN      CNAME   {{username}}.github.io
+    ```
--- test/generateCapData.js
+++ test/generateCapData.js
@@ -6,6 +6,8 @@ const demoDate = new Date("2017-01-01")
 
 describe('Generates demo ICO fundraising data', () => {
 
+    const ICOs = jsonfile.readFileSync(path.join(process.cwd(), 'public/res/sample-data.json'));
+
     it('Generates demo data in JSON format', (done) => {
         console.log(demoDate)
 
@@ -14,19 +16,23 @@ describe('Generates demo ICO fundraising data', () => {
         const set = [];
 
         // run for 30 days 
-        for (i = 0; i < 30; i++) {
-            set.push({
-                time: (new Date(startDay.add(1, 'days').format())).getTime(),
-                amount: Math.round(Math.random() * amountSeed * 100) / 100
-            });
-        }
+        let updatedICOs = ICOs.map(ico => {
+            ico.stats = [];
+            for (i = 0; i < 30; i++) {
+                ico.stats.push({
+                    time: (new Date(startDay.add(1, 'days').format())).getTime(),
+                    amount: Math.round(Math.random() * amountSeed * 100) / 100
+                });
+            }
+            return ico;
+        });
 
-        console.log('Simulated dataset => ', set);
+        console.log('Simulated dataset => ', updatedICOs);
 
         // write out data set
         const outputFile = path.join(process.cwd(), 'public', 'res', `sample-trend-${moment().format('YYYYMMDD_HHmmss')}.json`)
 
-        jsonfile.writeFileSync(outputFile, set);
+        jsonfile.writeFileSync(outputFile, updatedICOs);
 
         done()
     })
